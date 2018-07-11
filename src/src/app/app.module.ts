2cbf87c8776df5215e8453d289609367962efd20
Gianmarco <GSCordova7@gmail.com>
2018-07-11 14:36:40 +0200

    Add files via upload
    Añadiendo funcionalidades

---
+++ src/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Angular Material
+import { MyMaterialComponents } from './material.design-componentes';
+
+// Componentes
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import { GianmarcoComponent } from './components/gianmarco/gianmarco.component';
+
+// ModalFooter
+import { FooterModal } from './components/footer/footer.component';
+
+// Router
+import { AppRoutingModule } from './app-routing.module';
+
+// Hammer.js
+import 'hammerjs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterModal,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    GianmarcoComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MyMaterialComponents,
+    AppRoutingModule
+  ],
+  entryComponents: [AppComponent, FooterModal],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/src/app/components/gianmarco/gianmarco.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gianmarco-component',
+    templateUrl: './gianmarco.component.html'
+  })
+
+export class GianmarcoComponent { }
